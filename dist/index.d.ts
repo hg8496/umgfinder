5ebb40f9661488db4c7d47f73f73af560ac2dbdc
Christian Stolz <hg8496@cstolz.de>
2019-04-25 12:12:25 +0200

    Removed dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IDevice {
-    manufacturer: string;
-    deviceType: string;
-    firmware: string;
-    serialNumber: string;
-    deviceName: string;
-    ip: string;
-}
-export declare function scanNetwork(networkBlock: any): Promise<IDevice[]>;
